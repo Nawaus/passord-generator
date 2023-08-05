aeb89ae8bd43a54412d115460fafce0a770dc7e6
unknown <ggug9289@gmail.com>
2023-08-05 12:58:11 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
-import Generator from "../src/Generator/index";
+import styles from "../src/Generator/Generator.module.css";
+
+import { useState } from "react";
 
 function App() {
+  const [password, setPassword] = useState("");
+  const [CopyText, setCopyText] = useState("Copy");
+
+  function generate() {
+    const characters =
+      "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?";
+    const length = 12;
+    let newPassword = "";
+    for (let i = 0; i < length; i++) {
+      const position = Math.floor(Math.random() * characters.length);
+
+      newPassword += characters[position];
+    }
+    setPassword(newPassword);
+    setCopyText("Copy");
+  }
+
+  function copyPassword() {
+    navigator.clipboard.writeText(password);
+    setCopyText("Copied");
+  }
+
   return (
-    <div>
-      <Generator />
+    <div className="">
+      <h1>Password Generator</h1>
+      <div className={styles.button}>
+        <button onClick={generate}>Generate</button>
+        <button
+          onClick={() => {
+            copyPassword(password);
+          }}>
+          {CopyText}
+        </button>
+      </div>
+      <div>{password}</div>
     </div>
   );
 }
--- src/Generator/index.jsx
+++ src/Generator/index.jsx
@@ -1,108 +1,27 @@
 import styles from "../Generator/Generator.module.css";
 import { useState } from "react";
+const [password, setPassword] = useState("");
 
-const arrayWithLettersNumbersAndCharacters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "&",
-  "*",
-  "?",
-  "+",
-  "-",
-  "=",
-  "_",
-  "|",
-];
+function generate() {
+  const characters =
+    "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?";
+  const length = 12;
+  let newPassword = "";
+  for (let i = 0; i < length; i++) {
+    const position = Math.floor(Math.random() * characters.length);
 
-const generatePassword = (min, max) => {
-  const value = Math.floor(Math.random() * (max - min + 1)) + min;
-  const password = arrayWithLettersNumbersAndCharacters.slice(value);
-  return password;
-};
+    newPassword += characters[position];
+  }
+  setPassword(newPassword);
+}
 
 export default function Generator() {
-  const [password, setPassword] = useState("");
-  const [buttonText, setButtonText] = useState("Copy");
-
   return (
     <div className={styles.container}>
-      <h2 style={{ fontSize: "4em" }}>Password Generator</h2>
+      <h1>Password Generator</h1>
       <div className={styles.button}>
-        <button onClick={() => setPassword(generatePassword(0, 71))}>
-          Generate
-        </button>
-        <button
-          onClick={() => {
-            setButtonText("Copied!");
-            navigator.clipboard.writeText(password.join(""));
-          }}>
-          {buttonText}
-        </button>
+        <button onClick={generate}>Generate</button>
+        <button>Copy</button>
       </div>
       <div>{password}</div>
     </div>
